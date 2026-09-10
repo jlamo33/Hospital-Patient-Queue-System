1be7da50d700843a482fde0a74ccb4a75001f394
jlamo33 <jlamori@wgu.edu>
2026-09-10 18:33:25 -0400

    Added comments and added take patient, which removes the first patient in queue

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        Ward w1 = new Ward("Ward Joshua");
+        w1.admitPatient("John Doe", 1010, 1);
+        w1.admitPatient("John Deer", 0100, 2);
+        w1.takeNextPatient();
+        w1.printPatientQueue();
     }
 }
--- src/Ward.java
+++ src/Ward.java
@@ -12,27 +12,67 @@ public class Ward {
         this.wardName = wardName;
     }
 
+    public void printPatientQueue() {
+
+        Patient temp = head;
+        while(temp != null) {
+            System.out.print(temp.getPatientName() + " ID " + temp.getPatientID());
+            System.out.print("\t");
+            temp = temp.getNext();
+        }
+    }
+
+    private boolean isWardEmpty() {
+
+        return length == 0;
+    }
+
     public void admitPatient(String name, int ID, int severity) {
 
         // create a new Patient node
         Patient p1 = new Patient(name, ID);
 
-        if(length == 0) {
+        // check if the ward has zero patients
+        if(isWardEmpty()) {
 
             head = p1;
             tail = p1;
         }
 
-        // link nodes
-        tail.setNextPatient(p1);
-        p1.setPrevPatient(tail);
+        else {
 
-        // update the tail
-        tail = p1;
+            // link nodes
+            tail.setNextPatient(p1);
+            p1.setPrevPatient(tail);
+
+            // update the tail
+            tail = p1;
+
+        }
 
         length++;
 
     }
 
+    public void takeNextPatient() {
 
+        if(isWardEmpty()) {
+            return;
+        }
+
+        else if(length == 1) {
+            head = null;
+        }
+
+        else {
+
+            // remove link and update head to next patient in queue
+            Patient temp = head;
+            head = temp.getNext();
+            temp.setNextPatient(null);
+            length--;
+        }
+
+
+    }
 }

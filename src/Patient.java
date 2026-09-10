5e39165156cfe4dba53d818092de8056ae957812
jlamo33 <jlamori@wgu.edu>
2026-09-10 14:39:23 -0400

    Staged first patient class fields and constructor and getter methods

--- src/Patient.java
+++ src/Patient.java
@@ -1,4 +1,31 @@
 public class Patient {
 
+    // fields for node class (Patient class is a Node)
+
+    String patientName;
+    Patient next;
+    Patient prev;
+
+    // metrics , {Patient ID number , severity for priority queue, and there arrival time}
+    int severity;
+    int arrivalTime;
+    int patientID;
+
+
+    public Patient(String name, int ID) {
+
+        patientName = name;
+        patientID = ID;
+    }
     
+    public String getPatientName() {
+
+        return this.patientName;
+    }
+
+    public int getPatientID() {
+
+        return patientID;
+    }
+
 }
